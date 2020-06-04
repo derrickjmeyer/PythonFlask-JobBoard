8bf0f00da0cef52d7b5ffda56e54c4cac1cc9018
derrickjmeyer <derrick.meyer@jamf.com>
2020-06-04 16:32:00 +1000

    Base Templating and Styling

--- jobs/app.py
+++ jobs/app.py
@@ -3,14 +3,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-def main():
-    pass
-
 @app.route('/')
 @app.route('/jobs')
 def jobs():
     return render_template('index.html')
 
-
-if __name__ == '__main__':
-    main()
