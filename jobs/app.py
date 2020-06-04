2fee97c06890dd38c420aafb362f0fe2d986aca8
derrickjmeyer <derrick.meyer@jamf.com>
2020-06-04 10:11:58 +1000

    Flask Setup

---
+++ jobs/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+def main():
+    pass
+
+@app.route('/')
+@app.route('/jobs')
+def jobs():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    main()
